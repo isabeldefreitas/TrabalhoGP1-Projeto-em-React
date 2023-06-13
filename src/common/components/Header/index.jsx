b118833dd7feac023a9bc96a1da69c886d2c8498
MarianeSMagalhaes <marianesantosmagalhaes@hotmail.com>
2023-06-13 16:47:57 -0300

    Atualização do Header

--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -1,51 +1,47 @@
 import { useState } from "react";
-import { Link, useNavigate, Outlet } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiMicrochip, BiSearchAlt2, BiShoppingBag } from "react-icons/bi";
 
-import { Container, Logo } from "./styles";
+import { Button, Container, Form, Input, Item, Logo } from "./styles";
 
+export const Navbar = ({funcao}) => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-export const Navbar = () => {
-    const [search, setSearch] = useState("");
-    const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    if (!search) return;
 
-        if (!search) return;
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
 
-        navigate(`/search?q=${search}`, { replace: true });
-        setSearch("");
-    };
+  return (
+    <Container>
+      <Logo>
+        <button onClick={() => funcao()}>
+          <BiMicrochip /> HardwareTech
+        </button>
+      </Logo>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="Busque um produto"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <Button type="submit">
+          <BiSearchAlt2 />
+        </Button>
+      </Form>
+      <Item>
+        <Link to="/">
+          <BiShoppingBag />
+        </Link>
+      </Item>
 
-    return (
-        <Container>
-            
-                <Logo>
-                    <Link to="/">
-                        <BiMicrochip /> HardwareTech
-                    </Link>
-                </Logo>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Busque um produto"
-                        onChange={(e) => setSearch(e.target.value)}
-                        value={search}
-                    />
-                    <button type="submit">
-                        <BiSearchAlt2 />
-                    </button>
-                </form>
-                <div>
-                    <Link to="/">
-                        <BiShoppingBag />
-                    </Link>
-                </div>
-
-          
-            <Outlet />
-        </Container>
-    );
+      {/* <Outlet /> */}
+    </Container>
+  );
 };
-
--- src/common/components/Header/styles.js
+++ src/common/components/Header/styles.js
@@ -1,15 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 1400px;
-height: 55px;
-background-color: #121212;
-display: flex;
-justify-content: space-between;
-
+  width: 100%;
+  height: 55px;
+  background-color: #121212;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 export const Logo = styled.h2`
+  color: #fff;
+  margin-left: 10px;
+`;
 
+export const Item = styled.div`
+  color: #fff;
+  margin-right: 20px;
+`;
+
+export const Input = styled.input`
+  padding: 0.2rem 0.8rem;
+  border-radius: 4px;
+  border: none;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const Button = styled.button`
+  background-color: #bcffee;
+  border: 2px solid #b3e3ff;
+  border-radius: 4px;
+  color: #000;
+  padding: 0.3rem;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 `;
 
 // #navbar {
@@ -19,25 +48,25 @@ export const Logo = styled.h2`
 //     padding: 1rem 2rem;
 //     background-color: #121212;
 //   }
-  
+
 //   #navbar h2 a {
 //     display: flex;
 //     align-items: center;
 //     gap: 0.5rem;
 //     color:#bcffee;
 //   }
-  
+
 //   #navbar form {
 //     display: flex;
 //     gap: 0.5rem;
 //   }
-  
+
 //   #navbar input {
 //     padding: 0.2rem 0.8rem;
 //     border-radius: 4px;
 //     border: none;
 //   }
-  
+
 //   #navbar form button {
 //     background-color: #bcffee;
 //     border: 2px solid #b3e3ff;
