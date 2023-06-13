4f0c425581804819a711d10777d3c330ecdf17a1
MarianeSMagalhaes <marianesantosmagalhaes@hotmail.com>
2023-06-13 16:13:21 -0300

    Criação do MenuHamburguer

---
+++ src/common/components/MenuHamburguer/index.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import * as Styles from "./styles";
+
+export const MenuHamburguer = () => {
+  return (
+    <>
+      <Styles.Menu>
+        <ul>
+          <li>Opção </li>
+          <li>Opção </li>
+          <li>Opção </li>
+          <li>Opção </li>
+        </ul>
+      </Styles.Menu>
+    </>
+  );
+};
---
+++ src/common/components/MenuHamburguer/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Menu = styled.nav`
+  width: 100px;
+  height: 300px;
+  background-color: red;
+  position: absolute;
+
+  ul {
+    margin: 0;
+  }
+`;
