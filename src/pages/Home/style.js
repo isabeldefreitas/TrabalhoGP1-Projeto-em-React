03c37ed1074a7931547e6e5c12a429b3500d2ce5
isabeldefreitas <127349333+isabeldefreitas@users.noreply.github.com>
2023-06-13 16:41:13 -0300

    estilo Home

---
+++ src/pages/Home/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: black;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 500px;
+`;
