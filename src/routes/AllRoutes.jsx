7880ae904b5050cf6bc8c39cd69bc742443d5874
Larissa Rosa <127329662+larissrosa@users.noreply.github.com>
2023-06-13 16:43:15 -0300

    atualizei as rotas

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,23 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { AddProduct } from "../pages/AddProduct";
 import { LoginCliente } from "../pages/LoginCliente";
-// import { LoginFuncionario } from "../pages/LoginFuncionario";
-import { Navbar} from "../common/components/Header";
-
-import { Footer } from "../common/components/Footer";
 
 export function AllRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route Component={Navbar}>
-                    <Route Component={Footer}>
-                        <Route Component={Home} path="/" />
-                        <Route Component={AddProduct} path="/create" />
-                    </Route>
-                </Route>
-                <Route Component={LoginCliente} path="/loginCliente" />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={Home} path="/" />
+        <Route Component={LoginCliente} path="/loginCliente" />
+        <Route Component={AddProduct} path="/create" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
