f5c56eb8a9c35bd270fa96dd45b0ec2794dac2df
Larissa Rosa <127329662+larissrosa@users.noreply.github.com>
2023-06-14 18:00:50 -0300

    sei la

--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -3,14 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { BiMenu, BiMicrochip, BiSearchAlt2, BiShoppingBag } from "react-icons/bi";
 import { GlobalStyle } from '../../style/global';
 import { Button, Container, Form, Input, Item, Logo } from "./styles";
-import { Search } from "../../components/Search";
+//import { useProducts } from "../../../hooks/useProducts";
+// import { Search } from "../../components/Search";
 
 import { MenuHamburguer } from "../MenuHamburguer";
 
 export const Navbar = () => {
   const [search, setSearch] = useState("");
   const [menu, setMenu] = useState(false)
-
+  //const { products, setFilteredProducts } = useProducts()
   const showMenu = () => setMenu(!menu)
   const navigate = useNavigate();
 
@@ -25,40 +26,39 @@ export const Navbar = () => {
 
   return (
     <>
-    <GlobalStyle/>
-    <Container>
-      <Logo>
+      <GlobalStyle />
+      <Container>
+        <Logo>
           <BiMicrochip /> HardwareTec
-      </Logo>
-      <Form onSubmit={handleSubmit}>
-        <Search
-          // type="text"
-          // placeholder="Busque um produto"
-          // onChange={(e) => setSearch(e.target.value)}
-          // value={search}
-        />
-        <Button type="submit">
-          <BiSearchAlt2 />
-        </Button>
-      </Form>
-      <Item>
-        <Link to="/bag">
-          <BiShoppingBag size={35} />
-        </Link>
-      </Item>
-      <Item>
-      <Logo>
-        <Link onClick={() => showMenu()}>
-          <BiMenu size={40} />
-      
-        </Link>
-      </Logo>
-      </Item>
+        </Logo>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Busque um produto"
+            //onChange={(e) => setFilteredProducts(products.filter(product => product.nome.toLowerCase().includes(e.target.value.toLowerCase())))}
+          />
+          <Button type="submit">
+            <BiSearchAlt2 />
+          </Button>
+        </Form>
+        <Item>
+          <Link to="/bag">
+            <BiShoppingBag size={35} />
+          </Link>
+        </Item>
+        <Item>
+          <Logo>
+            <Link onClick={() => showMenu()}>
+              <BiMenu size={40} />
 
-      {menu && <MenuHamburguer active={setMenu}/>}
-      {/* <Outlet /> */}
+            </Link>
+          </Logo>
+        </Item>
 
-    </Container>
+        {menu && <MenuHamburguer active={setMenu}/>}
+        {/* <Outlet /> */}
+
+      </Container>
     </>
   );
 };
