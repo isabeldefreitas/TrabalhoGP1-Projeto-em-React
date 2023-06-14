ef5be88ccd1717a6cda71e1ed20cb478682744ee
luizitosuares <luizito.dev@gmail.com>
2023-06-13 21:09:17 -0300

    correção cards

--- src/pages/AddProduct/index.jsx
+++ src/pages/AddProduct/index.jsx
@@ -1,1 +1,24 @@
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import { api } from "../../services/api";
 
+export function AddProduct() {
+    
+    
+    
+    {users.map((res) => {
+         return (
+            
+<div key={res.idProduto}>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={`data:image/jpeg;base64,${res.imagem}`} width="170px" height="110px" />
+      <Card.Body>
+        <Card.Title>{res.nome}</Card.Title>
+        <Card.Text>
+        {res.descricao}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+   </div> )
+    })}}
