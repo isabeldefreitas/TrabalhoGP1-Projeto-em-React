8efe89343b2a348d009f3dd326ffe881cbf2684e
Larissa Rosa <127329662+larissrosa@users.noreply.github.com>
2023-06-15 00:48:41 -0300

    Finalização da bag

--- src/pages/Bag/index.jsx
+++ src/pages/Bag/index.jsx
@@ -12,12 +12,22 @@ export function Bag() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
   const [bagItens, setBagItens] = useState([]);
   const [username, setUsername] = useState("");
+  const [total, setTotal] = useState(0);
+
   useEffect(() => {
     localStorage.getItem("username") &&
       setUsername(localStorage.getItem("username"));
-    localStorage.getItem("bag") &&
-      setBagItens(JSON.parse(localStorage.getItem("bag")));
-  }, []);
+    if (localStorage.getItem("bag")) { 
+      const itens = JSON.parse(localStorage.getItem("bag"))
+      setBagItens(itens);
+      var valueBag = 0
+      itens.map((iten) => {
+        valueBag += iten.valorUnitario
+      })
+      const formatado = valueBag.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      setTotal(formatado)
+    }
+    }, [localStorage.getItem("bag")]);
 
   const handleClick = () => {
     setMenuIsVisible(!menuIsVisible);
@@ -29,64 +39,71 @@ export function Bag() {
     setBagItens(novoArray);
   };
 
+  const handleFinishPurchase = () => {
+    alert(`COMPRA FINALIZADA COM SUCESSO. Valor total:`+ total)
+    setBagItens([])
+    localStorage.setItem("bag", JSON.stringify([]))
+  }
+
   return (
     <Styles.Container>
       <GlobalStyle />
       <Navbar funcao={handleClick} />
 
       {bagItens?.length >= 1 ? (
-        <div style = {{display:'flex', flexDirection:'column', width: '100%', alignItems: 'center'}}>
-        <Styles.Content>
-          {bagItens?.map((product) => {
-            return (
-              <div key={product.idProduto} style={{ marginTop: "20px" }}>
-                <Card style={{ width: "18rem", height: "528px" }}>
-                  <Card.Img
-                    variant="top"
-                    src={`data:image/jpeg;base64,${product.imagem}`}
-                    width="170px"
-                    height="250px"
-                  />
-                  <Card.Body
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Card.Text style={{ fontSize: "14px" }}>
-                      <Card.Title>{product.nome}</Card.Title>
-                      {product.descricao}
-                    </Card.Text>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center' }}>
+          <Styles.Content>
+            {bagItens?.map((product) => {
+              return (
+                <div key={product.idProduto} style={{ marginTop: "20px" }}>
+                  <Card style={{ width: "18rem", height: "528px" }}>
+                    <Card.Img
+                      variant="top"
+                      src={`data:image/jpeg;base64,${product.imagem}`}
+                      width="170px"
+                      height="250px"
+                    />
+                    <Card.Body
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Card.Text style={{ fontSize: "14px" }}>
+                        <Card.Title>{product.nome}</Card.Title>
+                        {product.descricao}
+                      </Card.Text>
 
-                    <Card.Text>
-                      <h2>R${product.valorUnitario}</h2>
-                      <Button
-                        onClick={() => handleRemoveItem(product.idProduto)}
-                        style={{
-                          marginTop: "10px",
-                          width: "100%",
-                          backgroundColor: "#9370DB ",
-                          border: "#9370DB",
-                        }}
-                        variant="primary"
-                      >
-                        Remover do Carrinho
-                      </Button>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })}
-          
-        </Styles.Content>
-        <Styles.Button>Finalize sua compra</Styles.Button>
+                      <Card.Text>
+                        <h2>R${product.valorUnitario}</h2>
+                        <Button
+                          onClick={() => handleRemoveItem(product.idProduto)}
+                          style={{
+                            marginTop: "10px",
+                            width: "100%",
+                            backgroundColor: "#9370DB ",
+                            border: "#9370DB",
+                          }}
+                          variant="primary"
+                        >
+                          Remover do Carrinho
+                        </Button>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
+
+          </Styles.Content>
+          <p>{total}</p>
+          <Styles.Button onClick={() => handleFinishPurchase()}>Finalize sua compra</Styles.Button>
         </div>
       ) : (
         <Styles.Message>
-          Poxa, {username} ! Você ainda não adicionou nenhum item ao carrinho!
-          
+          Poxa, {username}! Você ainda não adicionou nenhum item ao carrinho!
+
         </Styles.Message>
       )}
       <Footer />
